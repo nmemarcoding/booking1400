af684c007845c6f9151e28629184aaa31ca3cec7
nmemarcoding <nmemarcoding@gmail.com>
2022-05-15 20:51:27 -0700

    hotel controlers fixed

--- api/controllers/hotel.js
+++ api/controllers/hotel.js
@@ -1,5 +1,5 @@
 import Hotel from "../models/Hotel.js";
-// import Room from "../models/Rooms.js";
+import Room from "../models/Rooms.js";
 
 export const createHotel = async(req, res, next) => {
     const newHotel = new Hotel(req.body);
