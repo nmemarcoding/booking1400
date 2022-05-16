fec8c0a560fe86ee1b0c763ebf83976eb67769d0
nmemarcoding <nmemarcoding@gmail.com>
2022-05-15 20:03:51 -0700

    hotel update routes created

--- api/routes/hotels.js
+++ api/routes/hotels.js
@@ -10,11 +10,21 @@ router.post("/", async(req, res) => {
 
     try {
         const saveHotel = await newHotel.save()
-        res.status(200).json("savedHotel")
+        res.status(200).json(saveHotel)
     } catch (err) {
         res.status(500).json
     }
 })
 
+// update
+router.put("/:id", async(req, res) => {
+
+    try {
+        const updatedHotel = await Hotel.findByIdAndUpdate(req.params.id, { $set: req.body }, { new: true })
+        res.status(200).json(updatedHotel)
+    } catch (err) {
+        res.status(500).json
+    }
+})
 
 export default router
