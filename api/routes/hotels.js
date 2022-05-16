e017a71bb8277add3e987025e945defbb1ac4a83
nmemarcoding <nmemarcoding@gmail.com>
2022-05-15 20:06:50 -0700

    delete hotel by id rout

--- api/routes/hotels.js
+++ api/routes/hotels.js
@@ -27,4 +27,15 @@ router.put("/:id", async(req, res) => {
     }
 })
 
+// delete
+router.delete("/:id", async(req, res) => {
+
+    try {
+        const updatedHotel = await Hotel.findByIdAndDelete(req.params.id, { $set: req.body })
+        res.status(200).json("Hotel has benn deleted")
+    } catch (err) {
+        res.status(500).json
+    }
+})
+
 export default router
