246c1ba37ef3b7afe127ed7543274ba32f8ee32c
nmemarcoding <nmemarcoding@gmail.com>
2022-05-17 10:34:54 -0700

    cors

--- client/src/hooks/useFetch.js
+++ client/src/hooks/useFetch.js
@@ -1,37 +1,39 @@
 import { useEffect, useState } from "react";
-import axios from "axios";
+import Axios from "axios";
 
 const useFetch = (url) => {
-  const [data, setData] = useState([]);
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState(false);
+    const [data, setData] = useState([]);
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState(false);
+    const axios = Axios.create({
+        baseURL: "https://booking1400.herokuapp.com/api",
+    })
+    useEffect(() => {
+        const fetchData = async() => {
+            setLoading(true);
+            try {
+                const res = await axios.get(url);
+                setData(res.data);
+            } catch (err) {
+                setError(err);
+            }
+            setLoading(false);
+        };
+        fetchData();
+    }, [url]);
 
-  useEffect(() => {
-    const fetchData = async () => {
-      setLoading(true);
-      try {
-        const res = await axios.get(url);
-        setData(res.data);
-      } catch (err) {
-        setError(err);
-      }
-      setLoading(false);
+    const reFetch = async() => {
+        setLoading(true);
+        try {
+            const res = await axios.get(url);
+            setData(res.data);
+        } catch (err) {
+            setError(err);
+        }
+        setLoading(false);
     };
-    fetchData();
-  }, [url]);
 
-  const reFetch = async () => {
-    setLoading(true);
-    try {
-      const res = await axios.get(url);
-      setData(res.data);
-    } catch (err) {
-      setError(err);
-    }
-    setLoading(false);
-  };
-
-  return { data, loading, error, reFetch };
+    return { data, loading, error, reFetch };
 };
 
 export default useFetch;
